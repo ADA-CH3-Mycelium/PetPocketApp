7e1b63ef5eabd165eeb767b030f8ad913b65bfd9
sappawrdn <sapaasapa8@gmail.com>
2026-05-29 02:57:21 +0800

    PetEmergency view and lower iOS deployment target to 18.0

---
+++ PetPocket/Features/PetEmergency/Views/PetEmergencyView.swift
@@ -0,0 +1,18 @@
+//
+//  PetEmergencyView.swift
+//  PetPocket
+//
+//  Created by Muhammad Saffa Wardana on 29/05/26.
+//
+
+import SwiftUI
+
+struct PetEmergencyView : View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PetEmergencyView()
+}
