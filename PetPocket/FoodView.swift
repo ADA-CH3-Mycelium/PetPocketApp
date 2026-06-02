87645eda62fdad6c097df8df488b9ed691f83e48
Samantha Joice <kyj.samantha@gmail.com>
2026-06-02 08:33:22 +0800

    dashboard and routine cards

---
+++ PetPocket/FoodView.swift
@@ -0,0 +1,18 @@
+//
+//  FoodView.swift
+//  PetPocket
+//
+//  Created by Samantha Joice Lugay on 01/06/26.
+//
+
+import SwiftUI
+
+struct FoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FoodView()
+}
---
+++ PetPocket/MasterModels.swift
@@ -0,0 +1,8 @@
+//
+//  MasterModels.swift
+//  PetPocket
+//
+//  Created by Samantha Joice Lugay on 01/06/26.
+//
+
+import Foundation
