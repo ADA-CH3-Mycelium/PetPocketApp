22ff1aae3f0a2ad540e9214957ade90b4a88303a
efilonaru <naufalyasir5467@gmail.com>
2026-06-03 09:53:10 +0800

    feat: auth

--- PetPocket/Secrets.swift
+++ PetPocket/Secrets.swift
@@ -5,3 +5,10 @@
 //  Created by Naufal Muafa on 03/06/26.
 //
 
+import Supabase
+import Foundation
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://zuycprszpmkjdusznpgd.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1eWNwcnN6cG1ramR1c3pucGdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzc4MDgsImV4cCI6MjA5NTkxMzgwOH0.bbZwuQW5HVjxWJii9eKIgEpyj6ZzNEkaYUVmBsoDAy4"
+)
